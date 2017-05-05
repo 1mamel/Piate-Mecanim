30009c942f951c02c3b497cdb3e0456d85e5302e
Kamil Pilch <mamlikk2@gmail.com>
2017-05-05 16:58:12 +0200

    first commit

---
+++ Assets/DoorActivator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorActivator : MonoBehaviour
+{
+
+    public GameObject DoorGameObject;
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = DoorGameObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open", true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open", false);
+        }
+    }
+}
